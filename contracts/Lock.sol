873b2632273e3e7f149bad2b4113ec6b62ad1d8d
akinwunmi-akinrimisi <akinolaakinrimisi@gmail.com>
2024-09-20 15:29:24 +0100

    declare state variables

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,34 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.27;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+// Import the ERC-721 interface and other utilities from OpenZeppelin
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
-
-    event Withdrawal(uint amount, uint when);
-
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+contract NFTMarketplace is ERC721, Ownable {
+    
+    uint256 public _tokenIdCounter;  // To track token IDs
+    uint256 public _numberOfMintedNFTs;  // To track total minted NFTs
+    uint256 public _marketplaceFee;  // Marketplace fee as a percentage (2 means 2%)
+    uint256 public _numberOfSellers;  // Tracks the number of unique sellers
+    uint256 public _numberOfSales;    // Tracks total number of NFT sales
+    address public _admin;  // Admin address for contract control
+    
+    struct Seller {
+        uint256 totalSales;    // Total number of NFTs sold by the seller
+        uint256 totalEarnings; // Total Ether earned by the seller
+        uint256 totalMintedNFTS; // Total number of minted NFTs
+        address[] listOfCustomers; // List of customer addresses
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    mapping(uint256 => string) public _tokenURIs;  // Mapping of tokenId to IPFS URIs
+    mapping(address => uint256) public _proceeds;  // Tracks proceeds for each seller
+    mapping(uint256 => uint256) public _tokenPrices;  // tokenId to sale price
+    mapping(uint256 => bool) public _listedTokens;  // tokenId to listing status (true if listed)
+    mapping(address => bool) public _authorizedMinters;  // Mapping of authorized minters
+    mapping(address => Seller) public _sellers;  // Mapping of seller addresses to their sales details
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
+    constructor() ERC721("NFT Marketplace", "NFTM") {
+        _admin = msg.sender;  // Assign the contract deployer as the admin
+        _marketplaceFee = 2;  // Default marketplace fee of 2%
     }
+
+   
 }
